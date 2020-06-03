697224431985e03f1546d3067b3c5094ceaaa61f
Daniel Paiva <danpaiva@live.com>
2020-06-03 11:04:14 -0300

    Prática 03 - Exercício 06

--- Lista03/src/pratica03exercicio06.js
+++ Lista03/src/pratica03exercicio06.js
@@ -6,6 +6,7 @@ document.write("O raio do círculo é: " + raio);
 
 //Cálculo da área: acírculo = π * (raio)2
 
-area = Math.PI * (raio * raio);
+area = Math.PI * (raio ** 2);
 
-document.write("<br>A área é de: "+ area);
+document.write("<br>")
+document.write("A área é de: "+ area);
