fec07b912b4c451c6974a560fd774fdd20ba5eed
Daniel Paiva <danpaiva@live.com>
2020-06-07 11:21:56 -0300

    Prática 03 - Exercício 07

--- Lista03/src/pratica03exercicio07.js
+++ Lista03/src/pratica03exercicio07.js
@@ -1,2 +1,12 @@
 var raio, volumeEsfera;
 
+raio = prompt("Informe o raio da esfera: ");
+
+document.write("O raio da esfera é de: " + raio);
+document.write("<br>")
+
+//vesfera = 4 / 3 * π * (raio)3.
+volumeEsfera = 4 / 3 * Math.PI * (raio ** 3);
+
+document.write("<br>");
+document.write("O volume da esfera é de: " + volumeEsfera);
