857cbc14d982bac8a1bf8b195773ae3fcc3a38bc
Daniel Paiva <danpaiva@live.com>
2020-06-07 18:59:42 -0300

    Prática 03 - Exercício 08

--- Lista03/src/pratica03exercicio08.js
+++ Lista03/src/pratica03exercicio08.js
@@ -2,20 +2,23 @@ var capitalInicial, taxaJuros, prazo, montante;
 
 capitalInicial = prompt("Informe o capital inicial: ");
 
-document.write("O capital inicial é de: " + capitalInicial);
+document.write("O capital inicial é de: R$ " + capitalInicial + " .");
 document.write("<br>");
 
-taxaJuros = prompt("Informe a taxa de juros: ");
+taxaJuros = prompt("Informe a taxa de juros em %: ");
 
-document.write("A taxa de juros é de " + taxaJuros);
+taxaJuros /= 100;
+
+document.write("A taxa de juros é de :" + taxaJuros + " .");
 document.write("<br>");
 
 prazo = prompt("Informe o tempo de investimento: ");
 
-document.write("O tempo de investimento é de : " + prazo);
+document.write("O tempo de investimento é de : " + prazo + " .");
 document.write("<br>");
 
 //M = C * (1 + i)t
 
 montante = capitalInicial * (1 + taxaJuros) ** prazo;
 
+document.write("O montante é de: R$ " + montante + " ."); 
